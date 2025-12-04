f6e4821d3eb6b1cbdd389e2279a35fa308cd3762
Semyon Maryasin <simeon@maryasin.name>
2025-12-05 02:18:40 +0300

    Initial commit

---
+++ shell.nix
@@ -0,0 +1,22 @@
+let
+  nixpkgs-esp-dev = builtins.fetchGit {
+    url = "https://github.com/mirrexagon/nixpkgs-esp-dev.git";
+
+    # Optionally pin to a specific commit of `nixpkgs-esp-dev`.
+    # rev = "<commit hash>";
+  };
+
+  pkgs = import <nixpkgs> { overlays = [ (import "${nixpkgs-esp-dev}/overlay.nix") ]; };
+in
+pkgs.mkShell {
+  name = "esp-project";
+
+  buildInputs = with pkgs; [
+    esp-idf-full
+  ];
+
+  shellHook = ''
+    # expose them to clangd
+    export CPATH=$(printf "%s:" ${pkgs.esp-idf-full}/components/*/include | sed 's/:$//')
+  '';
+}
